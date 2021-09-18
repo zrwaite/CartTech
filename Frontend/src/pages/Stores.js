6c39b02d764ca5ef586e860bc0c42e13d4c1d624
Arnav <65367548+arnavgupta03@users.noreply.github.com>
2021-09-18 15:58:27 -0400

    Fix infinite reload issue

--- Frontend/src/pages/Stores.js
+++ Frontend/src/pages/Stores.js
@@ -3,10 +3,30 @@ import StoreCard from "../components/StoreCard";
 
 function Stores() {
     const [stores, setStores] = useState([]);
+    const [storeThirds, setStoreThirds] = useState([]);
+    const [didLoad, setDidLoad] = useState(false);
 
     useEffect(() => {
-        fetch("http://carttech.tech/api/stores").then(response => response.json()).then(data => setStores(data.response.stores));
-    });
+            if (!didLoad)
+            {
+                fetch("http://carttech.tech/api/stores").then(response => response.json()).then(data => {
+                    setStores(data.response.stores);
+                    var threes = [];
+                    for (var i = 0, j = 0; i < stores.length; i ++) {
+                        if (i >= 3 && i % 3 === 0)
+                        {
+                            j++;
+                        }
+                        threes[j] = threes[j] || [];
+                        threes[j].push(stores[i]);
+                    }
+
+                    setStoreThirds(threes);
+                    console.log(storeThirds);
+                    return true;
+                }).then(info => setDidLoad(info));
+            }
+        }, [stores, storeThirds, didLoad]);
 
     return (
         <div id="stores" className="container">
@@ -16,21 +36,25 @@ function Stores() {
             <div className="row text-center my-3">
                 <p className="h3">Here are all the stores you can shop at!</p>
             </div>
-            <div className="row">
-                {stores.map(function(store, i) {
-                    return (
-                        <div className="col" key={i}>
-                            <StoreCard
-                                storeId={store.id}
-                                storeName={store.name}
-                                storeInfo={store.info}
-                                storeAvailable={store.available_carts}
-                                storeTotal={store.total_carts}
-                            />
-                        </div>
-                    );
-                })}
-            </div>
+            {storeThirds.map(function(storeList, j) {
+                return (
+                    <div className="row">
+                        {storeList.map(function(store, i) {
+                            return (
+                                <div className="col" key={i}>
+                                    <StoreCard
+                                        storeId={store._id}
+                                        storeName={store.name}
+                                        storeInfo={store.info}
+                                        storeAvailable={store.available_carts}
+                                        storeTotal={store.total_carts}
+                                    />
+                                </div>
+                            );
+                        })}
+                    </div>
+                );
+            })}
         </div>
     );
 }
