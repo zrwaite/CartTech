8cba10bdf2ed77d676ae1a88df726238cdbdb5ff
Arnav <65367548+arnavgupta03@users.noreply.github.com>
2021-09-18 13:08:22 -0400

    Add products pages

---
+++ Frontend/src/components/ProductCard.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function ProductCard(props) {
+    return (
+        <div id="productCard" className="card m-4 text-center">
+            <img src={"http://carttech.tech/files/" + props.productImage} alt={"Image of " + props.productName} className="img-fluid" />
+            <h3 className="display-3">{props.productName}</h3>
+        </div>
+    );
+}
+
+export default ProductCard;
