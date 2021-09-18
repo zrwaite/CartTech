349621a199285531fd67d926a9e7daff9bfef154
Zac Waite <129032699zw@gmail.com>
2021-09-18 15:05:31 -0400

    Added auth stuff for future

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -34,3 +34,21 @@ app.use("*", (req, res) => {
 });
 
 module.exports = app; //Export server for use in index.js
+/*
+const config = {
+    authRequired: false,
+    auth0Logout: true,
+    secret: process.env.AUTH0_SECRET || "dV-eMJbSYESCaySxRdTl8e3uDcoXV1b3Ge1dxy5wRqJ4DHzRux1B6hLS2b_vK7ps",
+    baseURL: 'http://localhost:2000',
+    clientID: 'C42bgZdesIAl0rkqgFf6vpY3DE4wFC3N',
+    issuerBaseURL: 'https://dev-it2no-e1.us.auth0.com'
+};
+app.use(auth(config));
+app.get('/', (req, res) => {
+    res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+});
+app.use("/callback", (req, res) => {
+    let result = new response(200, null, {"page": "callback"});
+    res.status(result.status).json(result); //Return 200 result
+});
+*/
