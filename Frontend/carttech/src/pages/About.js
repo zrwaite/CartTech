76a3762589cf1d2bbe42dc23f54ac2d6ec428250
arnavgupta03 <harshulgupta05@gmail.com>
2021-09-18 10:18:21 -0400

    added team photos and project description

--- Frontend/carttech/src/pages/About.js
+++ Frontend/carttech/src/pages/About.js
@@ -1,12 +1,14 @@
-import React from "react"
+import React from "react";
+import team from "../carttech-teamphoto.png";
+import '../styles/home.css'
 
 //making the home component
 function About() {
     //return the page
     return (
         <div id="about">
-            <h1 className="display-1">CartTech</h1>
-            <p className="lead">This is the about page.</p>
+            <img src={team} className="opacity-50 w-100 image" alt="team photo" />
+            <p>Grocery Shopping is a repetive, menial task, which means it is ripe for automation. Also, as we learned in the past 2 years, having these warehouse-like stores as a congregation point comes with many drawbacks. CartTech is here to make the process easier than ever.</p>
         </div>
     );
 }
