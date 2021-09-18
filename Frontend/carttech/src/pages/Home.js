ff5af77ea2400209c66a0331623dfc44c7e95485
Arnav <65367548+arnavgupta03@users.noreply.github.com>
2021-09-17 23:36:41 -0400

    Add create-react-app files to frontend

---
+++ Frontend/carttech/src/pages/Home.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+//making the home component
+function Home() {
+    //return the page
+    return (
+        <div>
+            <h1 className="display-1">CartTech</h1>
+            <p className="lead">This is the home page.</p>
+        </div>
+    );
+}
+
+//export the component
+export default Home;
---
+++ Frontend/carttech/src/pages/Register.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+//making the home component
+function Register() {
+    //return the page
+    return (
+        <div>
+            <h1 className="display-1">CartTech</h1>
+            <p className="lead">This is the register page.</p>
+        </div>
+    );
+}
+
+//export the component
+export default Register;
