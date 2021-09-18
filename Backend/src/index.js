f9f4fd86c6f8660a1edcf34a14c3bd08798a34ea
Zac Waite <129032699zw@gmail.com>
2021-09-18 07:18:56 -0400

    Created basic server

---
+++ Backend/src/index.js
@@ -0,0 +1,26 @@
+const app = require("./server.js"); //Runs server
+const env = require("dotenv");  //Allows pulling information from .env with process.env
+const mongoose = require('mongoose');
+
+// configurations
+env.config();
+const port = process.env.PORT || 2000;
+
+// mongodb connection
+mongoose.connect(
+    `${process.env.MONGO_URL}`,
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true
+    }
+).catch(err =>{
+    console.error(err.stack)
+    process.exit(1)
+}).then(() => {
+    // if connection is successful, log it and start the server
+    console.log('Database connected');
+    app.listen(port, () => {
+        console.log(`listening on port ${port}`)
+    });
+});
---
+++ Backend/src/server.js
@@ -0,0 +1,31 @@
+const express = require("express"); //Uses express to create a server
+const cors = require("cors"); //Uses cors library to avoid dealing with that BS
+const response = require('./models/response'); //Created pre-formatted uniform response
+const app = express();
+
+/* Server routing */
+
+// routes
+const storesRoute = require("./route/stores.route");
+const productsRoute = require("./route/products.route");
+const purchaseRoute = require("./route/purchase.route"); 
+const ordersRoute = require("./route/orders.route");
+const cartsRoute = require("./route/carts.route");
+
+// utilities
+app.use(cors()); 
+app.use(express.json()); //Json module is used to parse json I guess
+
+// api
+app.use("/api/stores", storesRoute);
+app.use("/api/products", productsRoute); 
+app.use("/api/purchase", purchaseRoute); 
+app.use("/api/orders", ordersRoute); 
+app.use("/api/carts", cartsRoute);
+
+app.use("*", (req, res) => {
+    let result = new response(404, ["Not Found"]);
+    res.status(result.status).json(result); //Return 404 result
+});
+
+module.exports = app; //Export server for use in index.js
