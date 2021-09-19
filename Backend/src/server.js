37c406d65f243ae1695600e5af09f78e73838337
Zac Waite <129032699zw@gmail.com>
2021-09-19 02:15:35 -0400

    Random edits

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -7,57 +7,34 @@ const env = require("dotenv");  //Allows pulling information from .env with proc
 const jwt = require('express-jwt');
 const jwks = require('jwks-rsa');
 
-
 //configs
 env.config();
 const authConfig = {
     authRequired: false,
-    auth0Logout: true,
-    secret: process.env.AUTH_SECRET,
-    baseURL: 'http://carttech.tech',
-    clientID: 'uNb9Ff8g5NKQDtINqVU7IBhlm5tH25UI',
-    issuerBaseURL: 'https://dev-it2no-e1.us.auth0.com'
+    auth0Logout: true, 
+    secret: process.env.CLIENT_APP_SECRET,
+    baseURL: 'http://localhost:2000',
+    clientID: 'kCh9RYZuSA7zuuZGhHzVFlgC6HkKH5Zl', //App client id
+    issuerBaseURL: 'https://dev-it2no-e1.us.auth0.com',
 };
-const jwtCheck = jwt({
-    secret: jwks.expressJwtSecret({
-        cache: true,
-        rateLimit: true,
-        jwksRequestsPerMinute: 5,
-        jwksUri: 'https://dev-it2no-e1.us.auth0.com/.well-known/jwks.json'
-    }),
-    audience: 'https://cart-tech',
-    issuer: 'https://dev-it2no-e1.us.auth0.com/',
-    algorithms: ['RS256'],
-    credentialsRequired: false
-});
 
 
 // utilities
 app.use(cors()); 
 app.use(express.json()); 
-app.use(jwtCheck);
 app.use(auth(authConfig));
-
-
-
-
 
 // routes
 const storesRoute = require("./route/stores.route");
 const productsRoute = require("./route/products.route");
-const purchaseRoute = require("./route/purchase.route"); 
 const ordersRoute = require("./route/orders.route");
 const cartsRoute = require("./route/carts.route");
 const filesRoute = require("./route/files.route");
 const userRoute = require("./route/user.route");
 
-
-
-
 // api
 app.use("/api/stores", storesRoute);
 app.use("/api/products", productsRoute); 
-app.use("/api/purchase", purchaseRoute); 
 app.use("/api/orders", ordersRoute); 
 app.use("/api/carts", cartsRoute);
 app.use("/api/user", userRoute);
